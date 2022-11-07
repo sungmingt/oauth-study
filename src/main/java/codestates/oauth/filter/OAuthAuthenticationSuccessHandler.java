f23562f0039c1a3c763efb9c5eaf8ee6787c179c
sungmingt <qotjdals8313@naver.com>
2022-11-07 20:59:04 +0900

    REFACTOR : modify di method

--- src/main/java/codestates/oauth/filter/OAuthAuthenticationSuccessHandler.java
+++ src/main/java/codestates/oauth/filter/OAuthAuthenticationSuccessHandler.java
@@ -2,29 +2,24 @@ package codestates.oauth.filter;
 
 import codestates.oauth.auth.PrincipalDetails;
 import codestates.oauth.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
 //OAuth 로그인 성공 시 JWT 발급을 위한 successHandler
 @Component
+@RequiredArgsConstructor
 public class OAuthAuthenticationSuccessHandler implements AuthenticationSuccessHandler{
 
     private final JwtUtil jwtUtil;
-
-    public OAuthAuthenticationSuccessHandler(JwtUtil jwtUtil) {
-        this.jwtUtil = jwtUtil;
-    }
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
