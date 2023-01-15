c431f1d1b0e02ed3558542029ee9afb0ddc629ef
sungmingt <qotjdals8313@naver.com>
2023-01-16 00:39:50 +0900

    add method in memberRepository

--- src/main/java/codestates/oauth/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package codestates.oauth.repository;
-
-import codestates.oauth.model.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-    Member findByUsername(String username);
-}
